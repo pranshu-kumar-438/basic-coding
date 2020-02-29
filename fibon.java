782beddaf042f7ad7f38214d65c08c288b6e104d
Pranshu Kumar <42460330+pranshu-kumar-438@users.noreply.github.com>
2020-02-29 14:29:47 +0530

    fibonacci series

---
+++ fibon.java
@@ -0,0 +1,16 @@
+class fibon
+{
+   static int fibonacci(int n)
+     {
+       if(n==0)
+           return 0;
+       if(n==1)
+           return 1;
+       else
+           return fibonacci(n-1)+fibonacci(n-2);
+     }
+public static void main(String args[])
+{
+  System.out.println(fibonacci(6));
+}
+}      
